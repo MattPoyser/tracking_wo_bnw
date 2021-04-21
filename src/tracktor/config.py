f36d9c70c63a89c1bca4c054eaee469d52b52e53
Matt <matthew.poyser@durham.ac.uk>
2021-04-21 16:45:21 +0100

    reid ready

--- src/tracktor/config.py
+++ src/tracktor/config.py
@@ -18,7 +18,8 @@ __C.ROOT_DIR = osp.abspath(osp.join(osp.dirname(__file__), '..', '..'))
 
 # Data directory
 # __C.DATA_DIR = osp.abspath(osp.join(__C.ROOT_DIR, 'data'))
-__C.DATA_DIR = "/hdd/PhD/drone/"
+# __C.DATA_DIR = "/hdd/PhD/drone/"
+__C.DATA_DIR = "/data/drones/"
 
 def get_output_dir(module):
   """Return the directory where experimental artifacts are placed.
